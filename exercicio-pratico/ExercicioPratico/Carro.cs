99e3a110c1e85987d6edf44c60dfa33b25d3ecce
Juan Serros <serros404@gmail.com>
2024-03-06 15:00:30 -0300

    Criando a classe Carro e instânciando

---
+++ exercicio-pratico/ExercicioPratico/Carro.cs
@@ -0,0 +1,43 @@
+﻿namespace ExercicioPratico
+    {
+    internal class Carro
+        {
+
+        public string? Modelo;
+        public string? Montadora;
+        public string? Marca;
+        public int Ano;
+        public int Potencia;
+
+        // Construtor dos campos acima
+        public Carro(string? modelo, string? montadora, string? marca, int ano, int potencia)
+            {
+            Modelo = modelo;
+            Montadora = montadora;
+            Marca = marca;
+            Ano = ano;
+            Potencia = potencia;
+            }
+
+
+        // Método Exibir para exibir as informações dos carros
+
+        public void Exibir()
+            {
+            Console.WriteLine($"Modelo: {this.Modelo}");
+            Console.WriteLine($"Montadora: {Montadora}");
+            Console.WriteLine($"Marca: {this.Marca}");
+            Console.WriteLine($"Ano: {this.Ano}");
+            Console.WriteLine($"Potência: {this.Potencia} KM/H");
+            Acelerar();
+
+        }
+
+
+        // Método Acelerar
+        public void Acelerar()
+            {
+            Console.WriteLine($"Acelerando o meu {this.Marca}");
+            }
+        }
+    }
--- exercicio-pratico/ExercicioPratico/Program.cs
+++ exercicio-pratico/ExercicioPratico/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExercicioPratico;
+
+Carro chevrolet = new("Sedan", "Chevrolet", "Onix", 2016, 110);
+Carro ford = new("SUV", "Ford", "EcoSport", 2018, 120);
+
+chevrolet.Exibir();
+Console.WriteLine();
+ford.Exibir();
